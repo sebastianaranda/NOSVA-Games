bdf47b4fe6c693d871c7ee05315516846c5b1b38
Sebastian Aranda <sebastian.aranda.12@gmail.com>
2021-09-07 19:31:11 -0300

    Agrego categoria videos y creo los chips de seleccion de categoria

---
+++ js/videos.js
@@ -0,0 +1,44 @@
+const videos = [
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 1
+    },
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 2
+    },
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 3
+    },
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 4
+    },
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 5
+    },
+    {
+        url: "https://www.youtube.com/watch?v=3I1ssfR3FRU",
+        titulo: "Video demo",
+        descripcion: "Descripcion del video",
+        categoria: "Demo",
+        id: 6
+    }
+]
